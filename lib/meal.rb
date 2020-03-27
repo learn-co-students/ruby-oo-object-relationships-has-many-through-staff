0e7cfeaa3f675b5bbe030c0aea1c0a1ff76aff6d
github username <github email address>
2020-03-26 21:23:24 -0400

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,32 @@
 class Meal
 
+	attr_accessor :waiter, :customer, :total, :tip
+
+	@@all = []
+
+	def initialize(waiter, customer, total, tip=0)
+		@waiter = waiter
+		@customer = customer
+		@total = total
+		@tip = tip
+		@@all << self
+	end
+
+	def self.all 
+		@@all 
+	end
+
+	def meals
+		Meal.all.select do |meal|
+			meal.waiter == self 
+		end
+	end
+
+	def best_tipper
+		best_tipped_meal = meals.max do |meal_a, meal_b|
+			meal_a.tip <=> meal_b.tip 
+		end
+		best_tipped_meal.customer
+	end
+
 end
